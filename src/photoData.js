c446cbda2dc0bdb76672a5e3dccfc146b3f0378b
Nate J.M <nate@natejonah.com>
2023-02-26 21:49:20 +0000

    Start populating photo data file

---
+++ src/photoData.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        "id": "gTX7loRSKlQ",
+        "city": "Berlin, Germany",
+        "photo": "berlin.jpg",
+        "source": "https://unsplash.com/photos/gTX7loRSKlQ",
+        "photographer": {
+            "name": "Flo Karr",
+            "profile": "https://unsplash.com/@flo_karr"
+        }
+    }
+]
