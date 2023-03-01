5e57fd27878abe81ee1b77c39a30465bff6fb085
Nate J.M <nate@natejonah.com>
2023-03-01 20:13:45 +0000

    Add some photos to data file

--- src/photoData.js
+++ src/photoData.js
@@ -1,5 +1,45 @@
 export default [
     {
+        "id": "dnPXORQYfjM",
+        "city": "Amsterdam, Netherlands",
+        "photo": "amsterdam.jpg",
+        "source": "https://unsplash.com/photos/dnPXORQYfjM",
+        "photographer": {
+            "name": "Bruno van der Kraan",
+            "profile": "https://unsplash.com/@brunovdkraan"
+        }
+    },
+    {
+        "id": "-xsFKKYt62U",
+        "city": "Atlanta, Georgia, USA",
+        "photo": "atlanta.jpg",
+        "source": "https://unsplash.com/photos/-xsFKKYt62U",
+        "photographer": {
+            "name": "Greg Keelen",
+            "profile": "https://unsplash.com/@greg_keel3n"
+        }
+    },
+    {
+        "id": "P3yyNuNY8n0",
+        "city": "Barcelona, Spain",
+        "photo": "barcelona.jpg",
+        "source": "https://unsplash.com/photos/P3yyNuNY8n0",
+        "photographer": {
+            "name": "Christian García",
+            "profile": "https://unsplash.com/@patuflinx"
+        }
+    },
+    {
+        "id": "xAkCM1tbUhY",
+        "city": "Bangkok, Thailand",
+        "photo": "bangkok.jpg",
+        "source": "https://unsplash.com/photos/xAkCM1tbUhY",
+        "photographer": {
+            "name": "Evan Krause",
+            "profile": "https://unsplash.com/@evankrause_"
+        }
+    },
+    {
         "id": "gTX7loRSKlQ",
         "city": "Berlin, Germany",
         "photo": "berlin.jpg",
