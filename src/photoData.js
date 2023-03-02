f2f682dedab20a868f489b2b2051f27212c101f8
Nate J.M <nate@natejonah.com>
2023-03-02 21:30:04 +0000

    Add more photo data

--- src/photoData.js
+++ src/photoData.js
@@ -48,5 +48,55 @@ export default [
             "name": "Flo Karr",
             "profile": "https://unsplash.com/@flo_karr"
         }
-    }
+    },
+    {
+        "id": "UO02gAW3c0c",
+        "city": "Paris, France",
+        "photo": "paris.jpg",
+        "source": "https://unsplash.com/photos/UO02gAW3c0c",
+        "photographer": {
+            "name": "John Towner",
+            "profile": "https://unsplash.com/@heytowner"
+        }
+    },
+    {
+        "id": "4CMq1GxDSPA",
+        "city": "Tokyo, Japan",
+        "photo": "tokyo.jpg",
+        "source": "https://unsplash.com/photos/4CMq1GxDSPA",
+        "photographer": {
+            "name": "Derch",
+            "profile": "https://unsplash.com/@derchlee"
+        }
+    },
+    {
+        "id": "GOqiZ8__bJ4",
+        "city": "Toronto, Canada",
+        "photo": "toronto.jpg",
+        "source": "https://unsplash.com/photos/GOqiZ8__bJ4",
+        "photographer": {
+            "name": "Juan Rojos",
+            "profile": "https://unsplash.com/@juanrojas"
+        }
+    },
+    {
+        "id": "x5icsOgc7Lw",
+        "city": "Vienna, Austria",
+        "photo": "vienna.jpg",
+        "source": "https://unsplash.com/photos/x5icsOgc7Lw",
+        "photographer": {
+            "name": "Leyre",
+            "profile": "https://unsplash.com/@leyy"
+        }
+    },
+    {
+        "id": "nHNHPvk544s",
+        "city": "Zürich, Switzerland",
+        "photo": "zurich.jpg",
+        "source": "https://unsplash.com/photos/nHNHPvk544s",
+        "photographer": {
+            "name": "Rico Reutimann",
+            "profile": "https://unsplash.com/@ricoreu"
+        }
+    },
 ]
