2c28e52a643473ffcd8a42e6f8aa747f65cc95d6
Nate J.M <nate@natejonah.com>
2023-03-05 01:08:05 +0000

    Add class to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header>
+        <header className="Header">
             <h1>City Capture</h1>
             <h2>Stunning images from around the world.</h2>
         </header>
