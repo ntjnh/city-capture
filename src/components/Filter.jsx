fe4f0179235f7c8ac9f26248a0ed951de52ef2c0
Nate J.M <nate@natejonah.com>
2023-03-05 01:08:52 +0000

    Tweak Filter component layout

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,29 @@
 export default function Filter() {
     return (
         <div className="Filter">
-          <div>
-            <p>Currently showing: <span id="current">All</span></p>
-            <button id="filter-button">Filter</button>
-          </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4 filter-info">
+                        <p>Currently showing: <span id="current">All</span></p>
+                    </div>
 
-          <ul className="options">
-            <li><a id="all" href="#">All</a></li>
-            <li><a id="africa" href="#">Africa</a></li>
-            <li><a id="asia" href="#">Asia</a></li>
-            <li><a id="australasia" href="#">Australasia</a></li>
-            <li><a id="europe" href="#">Europe</a></li>
-            <li><a id="n-america" href="#">North America</a></li>
-            <li><a id="s-america" href="#">South America</a></li>
-          </ul>
+                    <div className="col-8">
+                        <button className="filter-button" id="filter-button">Filter</button>
+                    </div>
+
+                    <div className="col-12 continent-menu">
+                        <ul className="options">
+                            <li><a className="active" id="all" href="#">All</a></li>
+                            <li><a id="africa" href="#">Africa</a></li>
+                            <li><a id="asia" href="#">Asia</a></li>
+                            <li><a id="australasia" href="#">Australasia</a></li>
+                            <li><a id="europe" href="#">Europe</a></li>
+                            <li><a id="n-america" href="#">North America</a></li>
+                            <li><a id="s-america" href="#">South America</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
