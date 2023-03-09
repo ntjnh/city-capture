e22d5e91b4faad10acc8067695928395ed24eacd
Nate J.M <nate@natejonah.com>
2023-03-09 02:02:48 +0000

    Add lightbox attributes to Photo component

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,9 +2,15 @@ export default function Photo(props) {
     return (
         <div className={`grid-item col-6 col-md-4 col-lg-3 ${props.continent}`}>
             <div className="photo">
-                <a href={`https://source.unsplash.com/${props.id}`} data-lightbox="cities" data-title={props.city}>
+                <a 
+                    data-alt={props.city}
+                    data-lightbox="cities" 
+                    data-title={`Photo by ${props.photographer}`}
+                    href={`https://source.unsplash.com/${props.id}`}
+                >
                     <img src={`https://source.unsplash.com/${props.id}`} alt={props.city} />
                 </a>
+
                 <span className="caption">{props.city}</span>
             </div>
         </div>
