81d298c5d51517fc6e21f37014e87a0277a53995
Nate <codebynate@gmail.com>
2025-02-19 23:09:53 +0000

    Connect to API and fetch photos for gallery

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react'
+const key = ''
+const url = 'https://api.unsplash.com'
+const getSinglePhoto = '/photos/'
+const clientId = `?client_id=${key}`
+
 export default function Photo(props) {
+    const endpoint = `${url}${getSinglePhoto}${props.id}${clientId}`
+    const [photoFull, setPhotoFull] = useState('')
+    const [photoThumb, setPhotoThumb] = useState('')
+
+    useEffect(() => {
+        async function fetchPhotoData() {
+            const response = await fetch(endpoint)
+
+            if (response.ok) {
+                const data = await response.json()
+
+                setPhotoFull(data.urls.regular)
+                setPhotoThumb(data.urls.small)
+            } else {
+                throw new Error(`Response status: ${response.status}`)
+            }
+        }
+        fetchPhotoData()
+    }, [])
+
     return (
         <div className={`grid-item col-6 col-md-4 col-lg-3 all ${props.continent}`}>
             <div className="photo">
@@ -6,9 +32,9 @@ export default function Photo(props) {
                     data-alt={props.city}
                     data-lightbox="all"
                     data-title={`Photo by ${props.photographer}`}
-                    href={`https://source.unsplash.com/${props.id}`}
+                    href={photoFull}
                 >
-                    <img src={`https://source.unsplash.com/${props.id}`} alt={props.city} />
+                    <img src={`${photoThumb}`} alt={props.city} />
                 </a>
 
                 <span className="caption">{props.city}</span>
