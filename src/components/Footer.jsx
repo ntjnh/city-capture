48a5a195c3aec38a5a317fd7dec12c5c6d4656f5
Nate <codebynate@gmail.com>
2025-07-07 22:44:23 +0100

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return <footer className="Footer">
+        <a
+            className="text-black text-decoration-underline"
+            href="https://nate-dev.com?utm_source=codebynate&utm_medium=web&utm_campaign=city_capture"
+            target="_blank"
+            rel="noreferrer"
+        >Nate</a> built this.
+    </footer>
+}
